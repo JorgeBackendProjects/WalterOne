34321bd13a1e2a79f5f7555700d1529cfe4c7f22
JorgeBackendProjects <jorgemugar12@hotmail.com>
2023-10-21 16:28:05 +0200

    a

--- src/python/players_module/walterplayers/skeleton_player.py
+++ src/python/players_module/walterplayers/skeleton_player.py
@@ -2,6 +2,8 @@
 This is a demo player that does nothing. It is used as a template for new players.
 """
 import os.path
+import sys
+from walterplayers.constants import Role
 
 from dotenv import load_dotenv
 
@@ -10,12 +12,62 @@ from walterplayers.constants import Action
 
 
 class MyPlayer(BasePlayer):
-    """ This is a demo player that does nothing. It is used as a template for
-    new players. """
+    """ Clase que hereda de la clase padre BasePlayer. """
 
+    """ Método padre de la clase que se ejecuta en cada turno, se encarga de llamar a todas las 
+    funciones de la clase para realizar comprobaciones y acciones en un determinado turno. """
     def choose_action(self, find_response):
+
+        self.comprobar()
+
         return Action.STOP, None
 
+    def comprobar(self):
+        if self.find_response.status.life >= 170:
+            self.compLife170()
+        
+    def compLife170(self, find_response):
+        ''' Se asigna el jugador con menor vida de la zona actual. '''
+        current_weakest_enemy = self.get_weakest_enemy(find_response.current_zone)
+
+        ''' En cada zona vecina buscar el enemigo con menor vida y comprobar entre ellos sus puntos 
+        de vida para obtener mediante una comparación el que menos vida tiene y a que zona moverse. '''
+        weakest_neighbours_zone = None
+        weakest_neighbours_enemy = None
+        
+        for zone in find_response.neighbours_zones:
+            weakest_enemy = self.get_weakest_enemy(zone)
+            if (weakest_neighbours_enemy == None or
+                weakest_enemy.life < weakest_neighbours_enemy.life):
+                weakest_neighbours_enemy = weakest_enemy
+                weakest_neighbours_zone = zone
+        
+        if current_weakest_enemy.life < weakest_neighbours_enemy.life:
+            return Action.ATTACK, current_weakest_enemy.id
+        
+        elif current_weakest_enemy.life > weakest_neighbours_enemy.life:
+            return Action.MOVE, weakest_neighbours_zone.zone_id
+
+        elif current_weakest_enemy == None:
+            print()
+        elif weakest_neighbours_enemy == None:
+            print()
+        elif weakest_neighbours_enemy == None 
+
+    def get_weakest_enemy(self, zone):
+        ''' The weakest enemy in zone. '''
+        min_life = sys.maxsize
+        weakest_enemy = None
+
+        for enemy in self._get_enemies_in_zone(zone):
+            if enemy.life <= min_life:
+                weakest_enemy = enemy
+                min_life = enemy.life
+
+        return weakest_enemy
+    
+    def _get_enemies_in_zone(self, zone):
+        return list(filter(lambda ia: Role.PLAYER == ia.role, zone.ias))
 
 def get_args():
     """ Get arguments from command line """
